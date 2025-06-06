82d4a19caedbc5baea04e98d7fa080905b0b5991
ParasRana123 <parasrana579@gmail.com>
2025-06-06 23:18:40 +0530

    Added the backend for user logout

--- ai-agent-ticket/controllers/user.js
+++ ai-agent-ticket/controllers/user.js
@@ -52,3 +52,20 @@ export const login = async (req , res) => {
         })
     }
 }
+
+export const logout = async (req , res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        if(!token) return res.status(401).json({ error: "Unauthorized" });
+        jwt.verify(token , process.env.JWT_SECRET , (err , decoded) => {
+            if(err) return res.status(401).json({ error: "Unauthorized" });
+        })
+
+        res.json({ message: "Logout Successful" });
+    } catch(error) {
+        res.status(500).json({
+            error: "SignUp failed",
+            details: error.message
+        })
+    }
+}
