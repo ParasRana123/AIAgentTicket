e8b2e061457b9d7c4a91a3234c136d7a437709bc
ParasRana123 <parasrana579@gmail.com>
2025-06-07 01:40:42 +0530

    Added user routes

--- ai-agent-ticket/index.js
+++ ai-agent-ticket/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import userRoutes from "./routes/user";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/auth" , userRoutes);
 
 mongoose
         .connect(process.env.MONGO_URI)
