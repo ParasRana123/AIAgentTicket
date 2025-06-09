982119a5e14fec945d1f7058322753cb27966c13
ParasRana123 <parasrana579@gmail.com>
2025-06-09 11:00:57 +0530

    added middleware for fetching a single ticket

--- ai-agent-ticket/controllers/ticket.js
+++ ai-agent-ticket/controllers/ticket.js
@@ -48,6 +48,32 @@ export const getTickets = async (req , res) => {
         }
         return res.status(200).json({ tickets });
     } catch (error) {
+        console.error("Error fetching tickets: " , error.message);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+export const getTicket = async (req , res) => {
+    try {
+        const user = req.user;
+        let ticket;
+
+        if(user.role !== "user") {
+            ticket = Ticket.findById(req.params._id)
+                  .populate("assignedTo" , ["email" , "_id"])
+        } else {
+            ticket = Ticket.findOne({
+                createdBy: user._id,
+                _id: req.params.id
+            }).select("title description status createdAt")
+        }
+
+        if(!ticket) {
+            return res.status(404).json({ message: "Ticket not found" });
+        }
+
+        return res.status(404).json({ticket});
+    } catch (error) {
         console.error("Error fetching ticket: " , error.message);
         return res.status(500).json({ message: "Internal Server Error" });
     }
