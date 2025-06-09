a50cc96fbedba24eddb339e03e698c79f84a57a0
ParasRana123 <parasrana579@gmail.com>
2025-06-09 11:14:30 +0530

    added all ticket routes

--- ai-agent-ticket/index.js
+++ ai-agent-ticket/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import {serve} from "inngest/express";
 import userRoutes from "./routes/user";
+import ticketRoutes from "./routes/ticket";
+import {inngest} from "./inngest/client";
+import { onUserSignUp } from "./inngest/functions/on-signup";
+import { onTicketCreated } from "./inngest/functions/on-create-ticket";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +15,13 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth" , userRoutes);
+app.use("/api/tickets" , ticketRoutes);
+
+// Route for inngest
+app.use("/api/inngest",serve({
+    client: inngest,
+    functions: [onUserSignUp , onTicketCreated]
+}))
 
 mongoose
         .connect(process.env.MONGO_URI)
--- ai-agent-ticket/routes/user.js
+++ ai-agent-ticket/routes/user.js
@@ -1,5 +1,14 @@
 import express from "express";
+import { getUsers, login, logout, signup, updateUser } from "../controllers/user";
+import { authenticate } from "../middlewares/auth";
 
 const router = express.Router();
+
+router.post("/update-user" , authenticate , updateUser);
+router.post("/get-user" , authenticate , getUsers);
+
+router.post("/signup" , signup);
+router.post("/login" , login);
+router.post("/logout" , logout);
 
 export default router;
